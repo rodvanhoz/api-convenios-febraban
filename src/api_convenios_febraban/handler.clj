e7058ee70576375639731ff37726208703a6ef07
Rodrigo Vanhoz Ribeiro <rodrigo.vanhoz@@gmail.com>
2021-09-17 23:46:47 -0300

    projeto criado

---
+++ src/api_convenios_febraban/handler.clj
@@ -0,0 +1,11 @@
+(ns api-convenios-febraban.handler
+  (:require [compojure.core :refer :all]
+            [compojure.route :as route]
+            [ring.middleware.defaults :refer [wrap-defaults site-defaults]]))
+
+(defroutes app-routes
+  (GET "/" [] "Hello World")
+  (route/not-found "Not Found"))
+
+(def app
+  (wrap-defaults app-routes site-defaults))
