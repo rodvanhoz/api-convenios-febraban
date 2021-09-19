59502981201420e2faba596e1cd57dbc3131f36c
Rodrigo Vanhoz Ribeiro <rodrigo.vanhoz@@gmail.com>
2021-09-18 23:11:52 -0300

    criando endpoint para buscar por segmento e por cod-convenio

--- test/api_convenios_febraban/controller_test.clj
+++ test/api_convenios_febraban/controller_test.clj
@@ -9,7 +9,7 @@
   (testing "should get all convenios"
     (with-redefs [db.febraban/get (fn [clauses] (mocks.database/mock-get clauses))]
       (let [result (get-all)]
-        (is (= (count result) 4))))))
+        (is (= (count result) 5))))))
 
 (deftest should-get-by-uuid
   (testing "should get by uuid"
@@ -31,4 +31,14 @@
         (is (= (-> result (first) (:segmento result)) "2"))
         (is (= (-> result (first) (:cod-convenio result)) "0040"))
         (is (= (-> result (first) (:nome result)) "DAERP - RIBEIRÃO PRETO"))
+        (is (= (-> result (first) (:uf result)) "SP")))))
+        
+  (testing "testing get by cod-convenio and segmento"
+    (with-redefs [db.febraban/get (fn [clauses] (mocks.database/mock-get clauses))]
+      (let [result (get-by-cod-convenio "0262" "5")]
+        (is (= (count result) 1))
+        (is (= (-> result (first) (:uuid result)) "eeec1d56-dbe6-4e64-91a4-5be9b8d3e2ed"))
+        (is (= (-> result (first) (:segmento result)) "5"))
+        (is (= (-> result (first) (:cod-convenio result)) "0262"))
+        (is (= (-> result (first) (:nome result)) "TESTE FEBRABAN SEGMENTO 5"))
         (is (= (-> result (first) (:uf result)) "SP"))))))
--- test/api_convenios_febraban/mocks/database.clj
+++ test/api_convenios_febraban/mocks/database.clj
@@ -5,7 +5,8 @@
 (def data (list {:uuid "50888255-1906-433e-b478-f9c48da8b9b7" :segmento "1" :cod_convenio "0000" :nome "Prefeitura Municipal de São Paulo" :uf "SP"}
                 {:uuid "782ba534-43dd-4995-a3f2-7879a1d4c551" :segmento "2" :cod_convenio "0040" :nome "DAERP - RIBEIRÃO PRETO" :uf "SP"}
                 {:uuid "3c2fd625-2540-46cf-909e-027b4c61803a" :segmento "4" :cod_convenio "0001" :nome "CETERP - CENTRAL TELEFÔNICA DE RIBEIRÃO PRETO" :uf "SP"}
-                {:uuid "f0b6fa79-e1da-4fd3-8676-d04f7164da35" :segmento "4" :cod_convenio "0262" :nome "NET RIBEIRÃO PRETO S.A" :uf "SP"}))
+                {:uuid "f0b6fa79-e1da-4fd3-8676-d04f7164da35" :segmento "4" :cod_convenio "0262" :nome "NET RIBEIRÃO PRETO S.A" :uf "SP"}
+                {:uuid "eeec1d56-dbe6-4e64-91a4-5be9b8d3e2ed" :segmento "5" :cod_convenio "0262" :nome "TESTE FEBRABAN SEGMENTO 5" :uf "SP"}))
 
 (defn- select-get [data]
   (doall (map (fn [v]
@@ -24,10 +25,16 @@
 (defn- mock-get-by-cod-convenio [cod-convenio]
   (select-get (filter #(= cod-convenio (:cod_convenio %)) data)))
 
+(defn- mock-get-by-cod-convenio-and-segmento [{:keys [cod_convenio segmento]}]
+  (let [result (filter #(= cod_convenio (:cod_convenio %)) data)]
+    (select-get (filter #(= segmento (:segmento %)) result))))
+
 (defn mock-get [clauses]
   (if (not-nil? (:uuid clauses))
     (mock-get-by-uuid (:uuid clauses))
     (if (not-nil? (:cod_convenio clauses))
-      (mock-get-by-cod-convenio (:cod_convenio clauses))
-        (select-get data))))
+      (if (not-nil? (:segmento clauses))
+        (mock-get-by-cod-convenio-and-segmento clauses)
+        (mock-get-by-cod-convenio (:cod_convenio clauses)))
+      (select-get data))))
   
